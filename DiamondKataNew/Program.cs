ad63847cfeea6baff1e1bd346165d5dd186c5e60
Manohar Lokare <lokare.manohar@outlook.com>
2022-09-13 03:24:20 +0530

    Delete file and remove the while loop

--- DiamondKataNew/Program.cs
+++ DiamondKataNew/Program.cs
@@ -2,21 +2,21 @@
 using DiamondKataNew;
 
 Console.WriteLine("Please enter a Character from A to Z");
-while (true)
-{
-    var input = Console.ReadLine();
-    if (input.Equals("exit", StringComparison.OrdinalIgnoreCase))
-    {
-        break;
-    }
-    else
-    {
+//while (true)
+//{
+//    var input = Console.ReadLine();
+//    if (input.Equals("exit", StringComparison.OrdinalIgnoreCase))
+//    {
+//        break;
+//    }
+//    else
+//    {
         char inputChar = Console.ReadKey().KeyChar;
         Console.WriteLine("\n");
 
         var diamond = Diamond.Create(inputChar);
         Console.WriteLine(diamond);
-    }
-}
+//    }
+//}
 
 
